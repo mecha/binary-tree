5604ddbbe232645f74b727448c402446347836a9
Miguel Muscat <miguelmuscat93@gmail.com>
2016-11-26 10:51:36 +0100

    Added `ChildAwareInterface`

---
+++ src/Node/ChildAwareNodeInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dhii\BinaryTree\Node;
+
+/**
+ * Represents a node that is aware of its child nodes.
+ *
+ * @since [*next-version*]
+ */
+interface ChildAwareNodeInterface
+{
+
+    /**
+     * Gets the left child node, if any.
+     *
+     * @return ReadableNodeInterface|null The left child node if it exists or null if not.
+     */
+    public function getLeftChild();
+
+    /**
+     * Gets the right child node, if any.
+     *
+     * @return ReadableNodeInterface|null The right child node if it exists or null if not.
+     */
+    public function getRightChild();
+
+}
